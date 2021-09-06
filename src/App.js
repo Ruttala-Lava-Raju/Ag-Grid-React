9c4170308b7c29f0511710b35e361c4d9ff5a905
Ruthala Lava Raju <88703564+Ruttala-Lava-Raju@users.noreply.github.com>
2021-09-06 18:00:22 +0530

    "My first commit"

--- src/App.js
+++ src/App.js
@@ -12,43 +12,66 @@ function App()
 		{headerName: "Title", field: "title", editable: true},
 		{headerName: "Description", field: "description", editable: true},
 		{headerName: "Learning Objectives", field: "objectives", editable: true},
-		{headerName: "Save", field: "syllabusNumber", cellRendererFramework: (params) =><button variant="primary" onClick={handleSave((params.value) - 1)}>Save</button>},
+		{headerName: "Save/Update", field: "syllabusNumber", cellRendererFramework: (params) =><button variant="primary" onClick={() => handleSave((params.value) - 1)}>Save/Update</button>},
 		{headerName: "Delete", field: "syllabusNumber", cellRendererFramework: (params) =><button variant="danger" onClick={() => handleDelete(params.value)}>Delete</button>},
 	];
 
-	const handleSave = (index) => {
-		console.log(index);
-		const syllabusItemClone = [...syllabusList];
-		// console.log(syllabusItemClone[index]);
-		onsave(syllabusItemClone[index]);
-	}
-	const onsave = (data) =>
+	const handleSave = (index) =>
 	{
-		console.log(data);
+		const syllabusItemsClone = [...syllabusList];
+		const syllabusItem = syllabusItemsClone[index];
+		console.log(syllabusItem.isUpdate);
+		// Axios.post("http://localhost:8002/api/syllabus", {
+		// 		"title": syllabusItem.title,
+		// 		"description": syllabusItem.description,
+		// 		"objectives": syllabusItem.objectives
+		// 	}).then((result) => {
+		// 		if(result.status === 201)
+		// 		{
+		// 			syllabusItemsClone[index] = result.data[0];
+		// 			console.log(result.data[0])
+		// 			mapSyllabusItems(syllabusItemsClone);
+		// 		}
+		// 	}).catch((error) =>
+		// 	{
+		// 		console.log(error);
+		// 	})
 	}
+
+	const mapSyllabusItems = (syllabusItems) =>
+	{
+		syllabusItems.map((syllabusItem, index) =>
+		{
+			syllabusItem["syllabusNumber"] = index + 1;
+			syllabusItem["isUpdate"] = true;
+			return(<></>);
+		})
+		setSyllabusList(syllabusItems);
+	}
+
 	const addEmptySyllabusForm = (event) => {
 		const syllabusItemsClone = [...syllabusList];
 		const emptySyllabusForm = {
 			title: null, 
 			description: null,
-			objectives: null
+			objectives: null,
+			isUpdate: false
 		};
 		syllabusItemsClone.push(emptySyllabusForm);
 		syllabusItemsClone.map((syllabusItem, index) =>
 		{
 			syllabusItem["syllabusNumber"] = index + 1;
+			return(<></>);
 		})
-		console.log(syllabusItemsClone);
 		setSyllabusList(syllabusItemsClone);
+		// mapSyllabusItems(syllabusItemsClone);
+		
 	};
 	useEffect(() => {
 		Axios.get()
 		.then((results) => {
 			const syllabusItems = results.data;
-			syllabusItems.map((syllabusItem, index) => {
-				syllabusItem["syllabusNumber"] = index + 1;
-			});
-			setSyllabusList(syllabusItems);
+			mapSyllabusItems(syllabusItems);
 		});
 	}, []);
 
@@ -57,16 +80,13 @@ function App()
 		const syllabusItemsClone = [...syllabusList];
 		index = index - 1;
 		const syllabusId = syllabusItemsClone[index].syllabusID;
-		Axios.delete("/" + syllabusId)
+		Axios.delete("http://localhost:8002/api/syllabus/" + syllabusId)
 		.then((result) =>
 		{
 			if(result.status === 200)
 			{
 				syllabusItemsClone.splice(index, 1);
-				syllabusItemsClone.map((syllabusItem, index) => {
-				syllabusItem["syllabusNumber"] = index + 1;
-			});
-				setSyllabusList(syllabusItemsClone);
+				mapSyllabusItems(syllabusItemsClone);
 			}
 		})
 		.catch((error) =>
