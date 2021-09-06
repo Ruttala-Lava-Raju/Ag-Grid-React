a78c4ecee030a6b4403c675a02fa83cd67eff3ab
Ruthala Lava Raju <88703564+Ruttala-Lava-Raju@users.noreply.github.com>
2021-09-06 16:01:07 +0530

    "My first commit"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
-import Login from './Login';
-import Course from './Course';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "./App.css";
+import Axios from "./axios";
+import React, {useEffect, useState} from "react";
+import { AgGridReact} from "ag-grid-react";
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
 function App()
 {
-	return (
-		<Router>
-			<Switch>
-				<Route exact path="/">
-					<Login/>
-				</Route>
-				<Route exact path="/Course">
-					<Course />
-				</Route>
-			</Switch>
-		</Router>
-	)
-}
-
+	const[syllabusList, setSyllabusList] =  useState([]);
+	const columnDefs = [
+		{headerName: "Syllabus Number", field: "syllabusNumber", editable: true},
+		{headerName: "Title", field: "title", editable: true},
+		{headerName: "Description", field: "description", editable: true},
+		{headerName: "Learning Objectives", field: "objectives", editable: true},
+		{headerName: "Save", field: "title", cellRendererFramework: (params) =><button variant="primary">Save</button>},
+		{headerName: "Delete", field: "title", cellRendererFramework: (params) =><button variant="danger">Delete</button>},
+	];
+	const addEmptySyllabusForm = (event) => {
+		const syllabusItemsClone = [...syllabusList];
+		const emptySyllabusForm = {
+			title: null, 
+			description: null,
+			objectives: null
+		};
+		syllabusItemsClone.push(emptySyllabusForm);
+		syllabusItemsClone.map((syllabusItem, index) =>
+		{
+			syllabusItem["syllabusNumber"] = index + 1;
+		})
+		console.log(syllabusItemsClone);
+		setSyllabusList(syllabusItemsClone);
+	};
+	useEffect(() => {
+		Axios.get()
+		.then((results) => {
+			const syllabusItems = results.data;
+			syllabusItems.map((syllabusItem, index) => {
+				syllabusItem["syllabusNumber"] = index + 1;
+			});
+			setSyllabusList(syllabusItems);
+		});
+	}, []);
+	const defaultColumnDefs = {
+		flex: 1,
+		sortable: true,
+		resizable: true
+	}
+	return(
+		<><h2 align="center">Online Courses</h2>
+		<button style={{"marginLeft": "1400px", "marginBottom": "20px"}} variant="primary" onClick={addEmptySyllabusForm}>Add Syllabus</button>
+		<div id="myGrid" className="ag-theme-alpine-dark" style={{height: "500px", "marginLeft": "20px", "marginRight":"20px"}}>
+			<AgGridReact 
+			reactUi="true"
+			rowData={syllabusList}
+			columnDefs={columnDefs}
+			defaultColDef = {defaultColumnDefs}
+			>
+			</AgGridReact>
+		</div>
+		</>
+	);
+};
 export default App;
